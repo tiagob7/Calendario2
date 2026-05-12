70c3d04d8810aa09431c538dc22a14c3e32bb4a7
tiagob7 <165058240+tiagob7@users.noreply.github.com>
2026-05-12 01:06:46 +0100

    Add files via upload

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -4,13 +4,12 @@
 // Evita inicializações duplicadas caso seja incluído em várias páginas
 if (!firebase.apps.length) {
   const firebaseConfig = {
-    apiKey: "AIzaSyC1ud2xlyldZ6qedVMuwe99QX4im6p_J20",
-    authDomain: "hub-algartempo.firebaseapp.com",
-    projectId: "hub-algartempo",
-    storageBucket: "hub-algartempo.firebasestorage.app",
-    messagingSenderId: "759072763920",
-    appId: "1:759072763920:web:80c5e56196eb4b7cd3da95",
-    measurementId: "G-M7SPWT4507"
+    apiKey: "AIzaSyBbTDfCCC9o5oxMSMRsvCUC50Iu6L0aqT8",
+    authDomain: "calendario-trabalho-39a6c.firebaseapp.com",
+    projectId: "calendario-trabalho-39a6c",
+    storageBucket: "calendario-trabalho-39a6c.firebasestorage.app",
+    messagingSenderId: "596069405321",
+    appId: "1:596069405321:web:275bb9da220206d3da6c58"
   };
 
   firebase.initializeApp(firebaseConfig);
